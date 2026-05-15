3d1fb80057ea79d0097b361077a33041c6ab39d2
tanjipon <tanjipon@gmail.com>
2026-05-15 15:36:22 +0800

    [fix][db] Migration 05: grant sequence permission to allow order_number generation

--- supabase/migrations/005_order_number.sql
+++ supabase/migrations/005_order_number.sql
@@ -1,2 +1,4 @@
 -- Add auto-increment order_number to orders table
 alter table orders add column order_number serial;
+
+grant usage, select on sequence orders_order_number_seq to anon, authenticated, service_role;
